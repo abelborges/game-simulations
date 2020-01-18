30322dca7165b28141dae4f902b0c4439860e652
Abel Borges <brgs.abel@gmail.com>
2020-01-18 18:05:35 -0300

    fix more bugs

--- uno/uno.py
+++ uno/uno.py
@@ -91,11 +91,16 @@ class Card:
         elif table.type == CARDS_BASIC:
             return table.color == self.color or table.number == self.number
         elif table.type in [CARDS_DIRECTION, CARDS_SKIP]:
-            return table.color == self.color
+            return table.color == self.color or table.type == self.type
         elif table.type in CARDS_JOKERS:
             return self.color == color
         elif table.type == CARDS_PLUS_2:
             return self.type == CARDS_PLUS_2 or self.color == table.color
+    
+    def __str__(self):
+        return '_'.join([str(a) for a in \
+            (self.type if self.type != CARDS_BASIC else None, \
+            self.color, self.number) if a is not None])
 
 class Player:
     
@@ -103,7 +108,7 @@ class Player:
         self.id = id
         self.cards = cards
     
-    def uno(self): return len(self.cards) == 0
+    def uno(self): return not bool(self.cards)
     
     def buy(self, cards):
         for card in cards: self.cards.append(card)
@@ -121,8 +126,7 @@ class Player:
         playable = self.playable(table, color)
         if not playable: return None
         
-        criteria = lambda c: c.is_joker()
-        playable.sort(key=criteria)
+        playable.sort(key=lambda c: c.is_joker())
         if not playable[0].is_joker():
             cards = [c for c in playable if not c.is_joker()]
         else:
@@ -144,6 +148,9 @@ class Player:
         if not colors:
             colors = [i for i in range(N_COLORS)]
         return random.choice(colors)
+    
+    def __str__(self):
+        return ','.join([str(c) for c in self.cards])
 
 class Game:
     
@@ -161,44 +168,34 @@ class Game:
         
         self.cards = cards
         self.players = players
-        self.table = [table]
         self.player_id = 0
+        self.table = [table]
+        
         self.buy = 0
         self.color = None
-        self.n_players = n_players
         self.direction = 1
+        self.finished = False
         
-        # debug
         self.debug = debug
         self.history = [{
-            "player_id": -1,
-            "card_id": table.id,
-            "card_type": table.type,
-            "card_color": table.color if table.color is not None else None,
-            "card_number": table.number if table.number is not None else -1,
-            "color": "" if self.color is None else self.color,
-            "n_cards": self.n_cards,
-            "next_cards": self.player_cards
+            "pid": -1,
+            "cid": table.id,
+            "cty": table.type,
+            "ccol": table.color if table.color is not None else "",
+            "cnum": table.number if table.number is not None else -1,
+            "col": "" if self.color is None else self.color,
+            "ns": self._n_cards()
         }]
     
     @property
-    def h(self): return pandas.DataFrame(self.history)
-    
-    @property
-    def n_cards(self): return ','.join([str(len(p.cards)) for p in self.players])
+    def n_players(self): return len(self.players)
     
     @property
     def table_top(self): return self.table[-1]
     
     @property
     def player(self): return self.players[self.player_id]
-
-    @property
-    def player_cards(self):
-        cards = ['_'.join([str(cc) for cc in (c.type if c.type != 'basic' else None, \
-            c.color, c.number) if cc is not None]) for c in self.player.cards]
-        return ','.join(cards)
-
+    
     def update_table(self):
         old_table = self.table.copy()
         random.shuffle(old_table)
@@ -223,9 +220,9 @@ class Game:
         self.direction *= (-1 if card.type == CARDS_DIRECTION else 1)
         plus = 2 if card.type == CARDS_SKIP else 1
         self.table.append(card)
-        self.update_player(plus)
         self.buy = buys(card)
         self.color = self.player.pick_color() if card.is_joker() else None
+        self.update_player(plus)
     
     def play_simple(self):
         card = self.player.play(self.table_top, self.color)
@@ -234,10 +231,10 @@ class Game:
             self.buy = 1
             bought_card = self.buy_cards().pop()
             if bought_card.is_playable(self.table_top, self.color):
+                self.player.cards.pop()
                 self.play(bought_card)
                 card = bought_card
-            else:
-                self.update_player()
+            else: self.update_player()
         else: self.play(card)
         return card
     
@@ -263,29 +260,9 @@ class Game:
             card = self.play_simple()
         return card
     
-    def uno(self): return self.player.uno()
-    
     def round(self):
-        """
-        - se carta simples, OK
-        - se +2
-            - se buy
-                - se tem outro +2
-                    - jogue
-                - else
-                    - compra 2
-            - se not buy
-                - age igual a carta simples
-        - se +4
-            - se buy
-                - compra 4
-            - else
-                - age igual a carta simples com color especificada
-        - se cor
-            - age igual a carta simples com color especificada
-        """
-        
-        if self.uno(): return
+        if self.finished:
+            return
         
         t = self.table_top
         player = self.player
@@ -294,19 +271,26 @@ class Game:
         elif t.type == CARDS_PLUS_4: card = self.play_plus4()
         else: card = self.play_simple()
         
-        # ---------- debug -------------------
-        self.history.append({
-            "player_id": player.id,
-            "card_id": card.id if card is not None else -1,
-            "card_type": card.type if card is not None else None,
-            "card_color": card.color if card is not None else None,
-            "card_number": -1 if card is None else \
-                (card.number if card.number is not None else -1),
-            "color": "" if self.color is None else self.color,
-            "n_cards": self.n_cards,
-            "next_cards": self.player_cards
-        })
-        # ------------------------------------
+        self.finished = player.uno()
         
-        if self.debug: print(self.h)
-        #return self.uno()
+        self.history.append({
+            "pid": player.id,
+            "cid": card.id if card is not None else -1,
+            "cty": card.type if card is not None else None,
+            "ccol": card.color if card is not None else None,
+            "cnum": -1 if card is None else \
+                (card.number if card.number is not None else -1),
+            "col": "" if self.color is None else self.color,
+            "ns": self._n_cards()
+        })
+        
+        if self.debug:
+            print(self._h())
+            print("\n")
+            for player in self.players:
+                print(str(player.id) + ": " + str(player))
+    
+    def _h(self): return pandas.DataFrame(self.history)
+    
+    def _n_cards(self):
+        return ','.join([str(len(p.cards)) for p in self.players])
